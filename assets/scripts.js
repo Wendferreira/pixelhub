fae9f559a6f2a365dfc015485e03b9ab5636f519
CiberCom <76117461+Wendferreira@users.noreply.github.com>
2026-03-30 13:30:10 -0300

    Create scripts.js

---
+++ assets/scripts.js
@@ -0,0 +1,5 @@
+// scripts.js
+document.addEventListener('DOMContentLoaded', function() {
+  console.log("O script foi carregado corretamente!");
+  // Aqui você pode adicionar a funcionalidade que desejar
+});
